b0b4bcee0d51b4dafa29868deea360cab8ae0537
naranjadigital <naranjasoftware@gmail.com>
2018-12-19 15:10:29 -0500

    Cambio en log LOGGER

--- logger.py
+++ logger.py
@@ -2,9 +2,8 @@ def log(debug):
     def _log(f):
         def wrap(a, b):
             if debug:
-                print('valor a:', a)
-                print('valor b:', b)
-
+                print('valor ejemplo a:', a)
+                print('valor ejemplo b:', b)
             return  f(a, b)
         return wrap
 
